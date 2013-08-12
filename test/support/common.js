0d3d69f09ba1fe6bed96ed10a9e053eb4c504622
Janez <mjanee@gmail.com>
2013-08-12 20:18:04 +0200

    added mongoose

--- test/support/common.js
+++ test/support/common.js
@@ -3,5 +3,6 @@ var mongoose = require('mongoose')
   , db = mongoose.connect(process.env.MONGO_DB_URI || 'mongodb://localhost/mongoose_troop')
 
 module.exports = {
-  db: db
+  db: db,
+  mongoose: mongoose
 }
